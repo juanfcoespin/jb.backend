4487d8b7daf88bf5462c655205b66babed2437e0
unknown <juanfco.espin@gmail.com>
2022-05-10 18:27:41 -0500

    10May2022: EntradaMercancia

---
+++ jbp.business.hana/EntradaMercanciaBusiness.cs
@@ -0,0 +1,25 @@
+﻿using jbp.msg.sap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jbp.business.hana
+{
+    public class EntradaMercanciaBusiness
+    {
+        public static string Ingresar(EntradaMercanciaMsg me)
+        {
+            try
+            {
+                var ms = "ok EM";
+                return ms;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
---
+++ jbp.services.rest/Controllers/EntradaMercanciaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using jbp.msg.sap;
+using jbp.business.hana;
+using System.Threading;
+
+
+namespace jbp.services.rest.Controllers
+{
+    public class EntradaMercanciaController : ApiController
+    {
+        [HttpPost]
+        [Route("api/EntradaMercancia")]
+        public string EntradaMercancia([FromBody]EntradaMercanciaMsg me)
+        {
+            var ms = EntradaMercanciaBusiness.Ingresar(me);
+            return ms;
+        }
+    }
+}
