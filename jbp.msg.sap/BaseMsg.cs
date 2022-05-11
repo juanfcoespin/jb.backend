91b63dd88b9a45992c722f0f2320f171b41d0cbb
juanfcoespin <juan.espin@yahoo.com>
2022-05-11 13:29:11 -0500

    base

---
+++ jbp.msg.sap/BaseMsg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jbp.msg.sap
+{
+    public class LineaMsg
+    {
+        public double Cantidad { get; set; }
+        public string CodArticulo { get; set; }
+    }
+}
